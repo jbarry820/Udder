64c8fb3d8e35c3e687ac92597fdc2a9cbbdb0104
Jim Barry <jbarry@pelicom.net>
2020-10-08 06:47:55 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by James Barry on 10/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
